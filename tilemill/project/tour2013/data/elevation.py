4faa9872e3b13b667281d799c5576881ec8f79cb
Alex Barth <alex@developmentseed.org>
2013-04-24 06:58:23 -0400

    Retrieve elevation data from geonames.

--- tilemill/project/tour2013/data/elevation.py
+++ tilemill/project/tour2013/data/elevation.py
@@ -2,7 +2,39 @@
 # http://www.geonames.org/export/web-services.html
 
 from fiona import collection
+import httplib
+import json
+import sys
 
-with collection("tracks.shp", "r") as input:
-    for point in input:
-        print point['geometry'] # point['id'] contains 
+for i in range(1, 22):
+    stage = "%02d" % i;
+    print "Retrieving elevation data for " + stage + ".shp/tracks.shp"
+    with collection(stage + ".shp/tracks.shp", "r") as input:
+        profile = []
+        for track in input:
+            for c in track['geometry']['coordinates']:
+                retries = 0
+                while True:
+                    try:
+                        conn = httplib.HTTPConnection("api.geonames.org")
+                        conn.request('GET', "/srtm3?lat=" + `c[1]` + "&lng=" + `c[0]` + "&username=lxbarth")
+                        response = conn.getresponse()
+                        if (response.status != 200):
+                            raise ValueError('Unexpected status')
+                        profile.append(int(response.read()))
+                        sys.stdout.write('.')
+                        sys.stdout.flush()
+                        break
+                    except ValueError:
+                        print "Unexpected error:", sys.exc_info()[0]
+                        retries = retries + 1
+                        if retries > 5:
+                            exit("Too many errors, aborting.")
+                        print "Retry ", retries
+                    finally:
+                        conn.close()
+
+        sys.sdout.write('\n')
+        filename = 'stage_' + stage + '_elevation.json';
+        json.dump(profile, open(filename, 'w'))
+        print filename + " written"
